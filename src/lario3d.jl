8bbf3fd467e0d8dbff62c9e52bf917c63c0b0002
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-10-13 14:54:10 +0200

    package in progress

---
+++ src/lario3d.jl
@@ -0,0 +1,11 @@
+module lario3d
+
+export version
+hello(who::String) = "Hello, $who"
+
+include("read.jl")
+function version():
+    return "0.0.2"
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using lario3d
+using Base.Test
+
+# write your own tests here
+@test 1 == 2
+
+@test hello("Julia") == "Hello, Julia"
