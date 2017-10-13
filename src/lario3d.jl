b5ba08447b910be67ece0e45647cb8a885d8cf54
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-10-13 15:13:43 +0200

    insert files into module

--- src/lario3d.jl
+++ src/lario3d.jl
@@ -4,6 +4,11 @@ export version
 hello(who::String) = "Hello, $who"
 
 include("read.jl")
+include("plasm.jl")
+include("surface.jl")
+include("representation.jl")
+include("import3d.jl")
+
 function version()
     return "0.0.2"
 end
