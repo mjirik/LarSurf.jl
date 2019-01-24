708f0b521709c7256143b6147d3fc594c68030cb
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-01-24 22:59:00 +0100

    sample data added to package

--- src/lario3d.jl
+++ src/lario3d.jl
@@ -1,17 +1,19 @@
 module lario3d
 
-export version
-hello(who::String) = "Hello, $who"
+    export version
+    hello(who::String) = "Hello, $who"
 
-include("read.jl")
-include("plasm.jl")
-include("surface.jl")
-include("representation.jl")
-include("import3d.jl")
-include("block.jl")
+    include("read.jl")
+    include("plasm.jl")
+    include("surface.jl")
+    include("representation.jl")
+    include("import3d.jl")
+    include("block.jl")
+    include("sampledata.jl")
+    include("io3d.jl")
 
-function version()
-    return "0.0.2"
-end
+    function version()
+        return "0.0.2"
+    end
 
 end
--- src/sampledata.jl
+++ src/sampledata.jl
@@ -1,28 +1,32 @@
-# hexaogon
-#    3 -- 4
-#  /  \ /  \
-# 1 -- 2 -- 5
-#  \  / \  /
-#   7 -- 8
-function hexagon()
-    hexagon_vertices = [0. 0.; 2. 0.; 1. 2.; 3. 2; 4. 0.; 3. -2.; 1. -2]
-    hexagon_faces = [ 1 2 3; 2 4 3; 4 2 5; 5 2 6; 2 7 6; 2 1 7]
+# module sampledata
 
-    return hexagon_vertices, hexagon_faces
-end
+    # hexaogon
+    #    3 -- 4
+    #  /  \ /  \
+    # 1 -- 2 -- 5
+    #  \  / \  /
+    #   7 -- 8
+    function hexagon()
+        hexagon_vertices = [0. 0.; 2. 0.; 1. 2.; 3. 2; 4. 0.; 3. -2.; 1. -2]
+        hexagon_faces = [ 1 2 3; 2 4 3; 4 2 5; 5 2 6; 2 7 6; 2 1 7]
 
-# Two tetrahedrons
-function two_tetrahedrons()
-    v = [1 1 1; 1 2 2; 2 2 1; 2 2 2; 2 3 2]
-    f = [1 2 3; 1 2 4; 1 3 4; 2 3 4;
-        2 3 4; 2 3 5; 2 4 5; 3 4 5]
+        return hexagon_vertices, hexagon_faces
+    end
 
-    return v, f
-end
+    # Two tetrahedrons
+    function two_tetrahedrons()
+        v = [1 1 1; 1 2 2; 2 2 1; 2 2 2; 2 3 2]
+        f = [1 2 3; 1 2 4; 1 3 4; 2 3 4;
+            2 3 4; 2 3 5; 2 4 5; 3 4 5]
 
-function tetrahedron()
-    # Tetrahedron
-    v = [1 1 1; 1 2 2; 2 2 1; 2 2 2]
-    f = [1 2 3; 1 2 4; 1 3 4; 2 3 4]
-    return v, f
-end
+        return v, f
+    end
+
+    function tetrahedron()
+        # Tetrahedron
+        v = [1 1 1; 1 2 2; 2 2 1; 2 2 2]
+        f = [1 2 3; 1 2 4; 1 3 4; 2 3 4]
+        return v, f
+    end
+
+# end
