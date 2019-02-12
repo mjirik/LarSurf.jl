78b70cf685364721c5fcce88860454b4f0be99b1
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-02-12 10:44:54 +0100

    boundary works not checked

--- examples/boundary_operator.jl
+++ examples/boundary_operator.jl
@@ -6,64 +6,79 @@ using Plasm
 using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
 
-V, CV = Lar.cuboidGrid([1,1,1])
+block_size = 2
+
+V, CV = Lar.cuboidGrid([block_size, block_size, block_size])
 
 # CV to FV
-sz = size(CV)[1]
+nc = size(CV)[1]
 nfaces_per_C = 6
-nfaces = nfaces_per_C * sz
-faces = Array{Int64}(undef, nfaces, 4)
+nfaces = nfaces_per_C * nc
+faces_i = Array{Int64}(undef, nfaces, 4)
 #     println(sz)
 # produce faces
-for k in 1:sz
+for k in 1:nc
     cube_points = CV[k]
-    faces[(k - 1) * nfaces_per_C + 1, :] = cube_points[[1, 2, 4, 3]]
-    faces[(k - 1) * nfaces_per_C + 2, :] = cube_points[[1, 5, 6, 2]]
-    faces[(k - 1) * nfaces_per_C + 3, :] = cube_points[[1, 3, 7, 5]]
-    faces[(k - 1) * nfaces_per_C + 4, :] = cube_points[[3, 4, 8, 7]]
-    faces[(k - 1) * nfaces_per_C + 5, :] = cube_points[[2, 6, 8, 4]]
-    faces[(k - 1) * nfaces_per_C + 6, :] = cube_points[[6, 5, 8, 7]]
+    faces_i[(k - 1) * nfaces_per_C + 1, :] = cube_points[[1, 2, 4, 3]]
+    faces_i[(k - 1) * nfaces_per_C + 2, :] = cube_points[[1, 5, 6, 2]]
+    faces_i[(k - 1) * nfaces_per_C + 3, :] = cube_points[[1, 3, 7, 5]]
+    faces_i[(k - 1) * nfaces_per_C + 4, :] = cube_points[[3, 4, 8, 7]]
+    faces_i[(k - 1) * nfaces_per_C + 5, :] = cube_points[[2, 6, 8, 4]]
+    faces_i[(k - 1) * nfaces_per_C + 6, :] = cube_points[[6, 5, 8, 7]]
 end
 
 # list of lists
 
-faces_list = [faces[k, :] for k=1:size(faces,1)]
+faces_list = [faces_i[k, :] for k=1:size(faces_i,1)]
 
 # Plasm.view(V, CV)
 # Plasm.viewexploded(V, CV)(2,2,2)
-Plasm.viewexploded(V, faces_list)(2,2,2)
+# Plasm.viewexploded(V, faces_list)(2,2,2)
 
-faces_t = convert(Array{Int64, 2}, faces')
+faces_t = convert(Array{Int64, 2}, faces_i')
 
 
 # convert from list of list to 2D array
 cv = Array{Int64}(undef, size(CV)[1], size(CV[1])[1])
 
-for k in 1:sz
+for k in 1:nc
     cv[k, :] = CV[k]
 end
 
 nvertices = size(V)[2]
 
 # CV to sparse
+print("CV to sparse, sz: ", nc, " nvertices: ", nvertices)
 
-CV01 = spzeros(Int8, sz, nvertices)
+CV01 = spzeros(Int8, nc, nvertices)
+nvertices_per_C = size(cv)[2]
 
-for k in 1:sz
-    for j in 1:nvertices
-        CV01[k, cv[k, j]] = 1
+for k in 1:nc
+    for i in 1:nvertices_per_C
+        # print("vert k: ", k,", i: ", i)
+        vertind = cv[k, i]
+        # print(" vert ind: ", vertind, "\n")
+        CV01[i, vertind] = 1
     end
 end
 
 # VF to sparse
+print("VF to sparse")
 nfaces = size(faces_t)[2]
 VF01 = spzeros(Int8, nvertices, nfaces)
 
 for k in 1:size(faces_t)[1]
     for i in 1:(size(faces_t)[2])
-        VF01[faces_t[k, i], k] = 1
+        # print("vert k: ", k,", i: ", i)
+        vertind = faces_t[k, i]
+        # print(" vert ind: ", vertind, "\n")
+        VF01[vertind, i] = 1
     end
 end
 
 
-boundary = CV01 * VF01
+boundary_numbers = CV01 * VF01
+
+# take just o
+
+boundary = boundary_numbers .== 4
