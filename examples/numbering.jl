f5c9b12dba9dcfb0d965d70cbea0cf88aad52c19
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-02-21 11:08:43 +0100

    see numbering to check

--- examples/numbering.jl
+++ examples/numbering.jl
@@ -2,9 +2,12 @@ using LinearAlgebraicRepresentation
 Lar = LinearAlgebraicRepresentation
 using Plasm, SparseArrays
 
+sz=[3, 3, 3]
+sz=[2, 3, 4]
 
-sz=[2,3,4]
-# V,(VV,EV,FV,CV) = Lar.cuboidGrid([sz[1], sz[2], sz[3]], true)
-V,(VV,EV,FV,CV) = Lar.cuboidGrid([sz[1], sz[2], sz[3]], false)
+
+# V,(VV,EV,FV,CV) = Lar.cuboidGrid([3,3,3], true)
+V,(VV,EV,FV,CV) = Lar.cuboidGrid(sz, true)
+
 # Plasm.view( Plasm.numbering(.6)((V,[VV, EV, FV])) )
-# Plasm.view( Plasm.numbering(.6)((V,[VV, EV, FV])) )
+Plasm.view( Plasm.numbering(.6)((V,[VV, EV, FV])) )
