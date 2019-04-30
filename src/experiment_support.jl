8549be100acf33fad9c8d0333ac190ca3464de9a
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-04-30 22:18:07 +0200

    do wrap on all or none

--- src/experiment_support.jl
+++ src/experiment_support.jl
@@ -7,33 +7,30 @@ julia> using lario3d
 julia> lario3d.add_to_csv("file.csv", (:first=>[4, 5], :second=>["a", "b"]))
 """
 function add_to_csv(df::Tuple, data_file::AbstractString)
-    println(typeof(df))
-    println(df)
     dfo = Dict(df)
-
-    println(typeof(dfo))
-    # println("adfa: ", dfo[])
     add_to_csv(dfo, data_file)
 end
 
 function add_to_csv(df::Dict, data_file::AbstractString)
     # wrap values into array
-    println("dict  ")
+    wrap_with_array = false
+    for (key, val) in df
+        if !isa(val, Array)
+            wrap_with_array = true
+            break
+        end
+    end
 
     new_dict = Dict()
     for (key, val) in df
-
-
-        println("key ", key, ", val:", df[key])
-        new_dict[key] = val
-
-        if !isa(df[key], Array)
+        if wrap_with_array
             new_dict[key] = [val]
+        else
+            new_dict[key] = val
         end
     end
 
 
-    println("df: ", new_dict)
     dfo = Pandas.DataFrame(new_dict)
     add_to_csv(dfo, data_file)
 
