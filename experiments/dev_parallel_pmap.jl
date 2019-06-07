5dc8ce70416328e56b7264bba82a581cecb5445e
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-07 12:58:32 +0200

    rename funcs

--- experiments/dev_parallel_pmap.jl
+++ experiments/dev_parallel_pmap.jl
@@ -3,16 +3,15 @@ if nprocs() == 1
     addprocs(3)
 end
 
-function bigfunc(iterator)
+function surface_extraction(iterator)
   localvar = 2.5
-
-  smallfunc = function (elm)
+  block_func = function (elm)
     # hypothetical (expensive) operation with elm
-    # (in my code this is solving a linear system)
+    # (in my code this is solving a block)
     localvar * elm
   end
 
-  pmap(smallfunc, iterator)
+  pmap(block_func, iterator)
 end
 
-bigfunc(1:10)
+surface_extraction_mock(1:10)
