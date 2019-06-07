a835aa8f3d604edc19c7a671989f92f5de81916b
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-07 23:31:20 +0200

    sample with stored matrix across all cpu

--- devel/dev_parallel_pmap_threedot.jl
+++ devel/dev_parallel_pmap_threedot.jl
@@ -3,6 +3,15 @@ if nprocs() == 1
     addprocs(3)
 end
 
+mat = Dict()
+println("----------------alskdjflaskfjasklf laksjflaskj-=-------")
+mat["5x5x5"] = [1,2,3]
+mat["15x15x15"] = [5,6]
+
+matice = @spawn mat;
+# mat[1] = 7
+# matice = @spawn mat;
+
 @everywhere function used_in_smallfunc(par1, par2)
   println(par1, par2, " ")
   return par1 + par2
@@ -15,7 +24,22 @@ function bigfunc(iterator)
   smallfunc = function (elm)
     # hypothetical (expensive) operation with elm
     # (in my code this is solving a linear system)
-    localvar * elm
+    vysledek = localvar * elm
+    global matice
+
+    # println("$localvar * $elm = $vysledek ")
+    # matice
+    # print("pred fetch $matice")
+    # fetch(matice);
+    # print("po fetch $matice")
+    local_matrix_storage = fetch(matice)
+    print(local_matrix_storage)
+    if local_matrix_storage["5x5x5"][1] == 1
+      local_matrix_storage["5x5x5"][1] = 2
+      matice = @spawn local_matrix_storage
+    else
+      print(" else...")
+    end
     # used_in_smallfunc(pars...)  # LoadError
     used_in_smallfunc(pars[1], pars[2])
   end
