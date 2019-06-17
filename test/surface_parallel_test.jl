ac5ab8cdf863bb47d47a1325401b47fd7f75bcb6
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-17 21:59:35 +0200

    make test simple

--- test/surface_parallel_test.jl
+++ test/surface_parallel_test.jl
@@ -42,40 +42,8 @@ end
     # end
 
     @debug "Setup done"
-    n, bgetter = LarSurf.lsp_setup_data(segmentation)
-    @async LarSurf.lsp_job_enquing(n, bgetter)
-    results = RemoteChannel(()->Channel{Array}(32));
+    LarSurf.lsp_get_surface(segmentation)
 
-    data_size = LarSurf.size_as_array(size(segmentation))
-
-    for wid in workers()
-        remote_do(
-            LarSurf.lsp_do_work_code_multiply_decode,
-            wid,
-            data_size,
-            LarSurf._ch_block,
-            results,
-        )
-    end
-
-    # print("===== Output Faces =====")
-    numF = LarSurf.grid_number_of_faces(data_size)
-
-    bigFchar = spzeros(Int8, numF)
-    @elapsed for i=1:n
-        @debug "Collecting the information for block $i"
-        faces_per_block = take!(results)
-        println(faces_per_block)
-        # for  block_i=1:block_number
-            for big_fid in faces_per_block
-                # median time 31 ns
-                # bigFchar[big_fid] = (bigFchar[big_fid] + 1) % 2
-                # median time 14 ns
-                bigFchar[big_fid] = if (bigFchar[big_fid] == 1) 0 else 1 end
-            end
-        # end
-
-    end
 
 end
 
