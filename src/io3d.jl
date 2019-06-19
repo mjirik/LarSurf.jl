6f745bd6b128499386724e60decc37765a8ab842
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-19 12:18:31 +0200

    new PyCall style

--- src/io3d.jl
+++ src/io3d.jl
@@ -4,13 +4,13 @@ using PyCall
 function read3d(fn)
     pyio3d = pyimport("io3d")
 
-    return pyio3d["read"](fn);
+    return pyio3d."read"(fn);
 end
 
 function datasets_join_path(pth)
     pyio3d_datasets = pyimport("io3d.datasets")
     print(pth)
-    opth = pyio3d_datasets["join_path"](pth, get_root=true)
+    opth = pyio3d_datasets."join_path"(pth, get_root=true)
     print(opth)
     return opth
 end
@@ -19,7 +19,7 @@ end
 function datasets_download(label)
     pyio3d_datasets = pyimport("io3d.datasets")
 #     print(pth)
-    pyio3d_datasets["download"](pth)
+    pyio3d_datasets."download".(pth)
 #     print(opth)
 #     return opth
 end
