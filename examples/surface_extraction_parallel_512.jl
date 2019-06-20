31f38893b63347dcc9535ae850c3ee01a8668c84
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-21 00:04:19 +0200

    measure prepared for exsu

--- examples/surface_extraction_parallel_512.jl
+++ examples/surface_extraction_parallel_512.jl
@@ -8,6 +8,11 @@ end
 using Test
 using Logging
 using SparseArrays
+# using ExSu
+
+
+fn = "exp_surface_extraction_cube_times.csv"
+data = Dict()
 
 @info "before everywhere using"
 @info "time from start: $(time()-time_start) [s]"
@@ -15,30 +20,51 @@ using SparseArrays
 @everywhere using LarSurf
 @everywhere using Distributed
 
+
 block_size = [64, 64, 64]
 data_size1 = 256
 data_size1 = 512
 
+LarSurf.set_time_data(data)
+
+data["nprocs"] = nprocs()
+# data["fcn"] = String(Symbol(fcni))
+data["nprocs"] = nprocs()
+data["nworkers"] = nworkers()
+data["jlfile"] = @__FILE__
+data["hostname"] = gethostname()
+data["ncores"] = length(Sys.cpu_info())
+data["data size"] = data_size1
+data["block size"] = block_size[1]
+
+data["using done"] = time()-time_start
 # segmentation = LarSurf.data234()
 @info "Generate data..."
 @info "time from start: $(time()-time_start) [s]"
 segmentation = LarSurf.generate_cube(data_size1; remove_one_pixel=true)
-@info "time from start: $(time()-time_start) [s]"
+@info "==== using done, data generated time from start: $(time()-time_start) [s]"
+data["data generated"] = time()-time_start
 
 @info "Setup..."
 setup_time = @elapsed LarSurf.lsp_setup(block_size;reference_time=time_start)
 println("setup time: $setup_time")
-@info "time from start: $(time()-time_start) [s]"
+@info "==== setup done, time from start: $(time()-time_start) [s]"
+data["setup done"] = time()-time_start
 # for wid in workers()
 #     # println("testing on $wid")
 #     ftr = @spawnat wid LarSurf._single_boundary3
 #     @test fetch(ftr) != nothing
 # end
 
-@debug "Setup done"
-val, tm, mem, gc = @timed LarSurf.lsp_get_surface(segmentation)
+# @debug "Setup done"
+tmd = @timed LarSurf.lsp_get_surface(segmentation)
+val, tm, mem, gc = tmd
 println("Total time: $tm")
+@info "==== finished, time from start: $(time()-time_start) [s]"
+data["finished"] = time()-time_start
+# using ExSu
+# ExSu.datetime_to_dict!(data)
+# ExSu.add_to_csv(data, fn)
 
-@info "time from start: $(time()-time_start) [s]"
 
 # Plasm.view(val)
