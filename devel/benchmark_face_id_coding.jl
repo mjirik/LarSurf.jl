ccf1dc7e27f6c6af2816ad98a9c232763592af09
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-23 00:38:14 +0200

    benchmark voxel ID

--- devel/benchmark_face_id_coding.jl
+++ devel/benchmark_face_id_coding.jl
@@ -1,15 +1,23 @@
+using Revise
 using BenchmarkTools
+using LarSurf
 
 function get_block_cart_calcul(blocks_number_axis, block_i)
 
-    x = div(block_i, blocks_number_axis[1] * blocks_number_axis[2])
+    z = div(block_i, blocks_number_axis[1] * blocks_number_axis[2])
     rest = mod(block_i, blocks_number_axis[1] * blocks_number_axis[2])
     y = div(rest, blocks_number_axis[1])
-    rest = mod(rest, blocks_number_axis[1])
-    z = rest
+    rest = mod(rest, blocks_number_axis[2])
+    x = rest
     return [x,y,z]
 end
 
+bl1 = get_block_cart_calcul([10,10,10], 125)
+println(bl1)
+bl2 = get_block_cart_cartind([10,10,10], 125)
+println(bl2)
+bl3 = LarSurf.grid_voxel_id_to_cart([10,10,10], 125)
+println(bl3)
 
 function get_block_cart_cartind(blocks_number_axis, block_i)
     a = Array{Int}(
@@ -42,15 +50,15 @@ function get_block_cart_cartind_extern(blocks_number_axis, block_i)
 end
 
 
+
 bm = @benchmark get_block_cart_cartind([10,10,10], 472)
-@info "$bm"
-println(bm)
+display(bm)
 
 bm = @benchmark get_block_cart_calcul([10,10,10], 472)
-@info "$bm"
+display(bm)
+
+bm = @benchmark LarSurf.grid_voxel_id_to_cart([10,10,10], 472)
+display(bm)
 
 bm = @benchmark get_block_cart_cartind_extern([10,10,10], 472)
-
-
-bl1 = get_block_cart_calcul([10,10,10], 472)
-bl2 = get_block_cart_cartind([10,10,10], 472)
+display(bm)
