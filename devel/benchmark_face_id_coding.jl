610da059d201b1e702415b531fef4a04c07bec98
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-23 08:07:29 +0200

    benchmark better prints

--- devel/benchmark_face_id_coding.jl
+++ devel/benchmark_face_id_coding.jl
@@ -1,25 +1,25 @@
-using Revise
 using BenchmarkTools
 using LarSurf
 
-function get_block_cart_calcul(blocks_number_axis, block_i)
-
-    z = div(block_i, blocks_number_axis[1] * blocks_number_axis[2])
-    rest = mod(block_i, blocks_number_axis[1] * blocks_number_axis[2])
-    y = div(rest, blocks_number_axis[1])
-    rest = mod(rest, blocks_number_axis[2])
-    x = rest
-    return [x,y,z]
+# function grid_voxel_id_to_cart(grid_size::Array{Int, 1}, ind::Int64)
+function grid_voxel_id_to_cart_calcul(grid_size, ind)
+#     ind = (sz2 * sz3) * (i - 1)  + (j - 1) * sz3 + k
+    sz1,sz2,sz3 = grid_size
+    layer = sz2*sz3
+    ir = div(ind - 1, layer) + 1
+    rest1 = mod(ind - 1 , layer) + 1
+#     println("rest1 ", rest1)
+    row = sz3
+    jr = div(rest1 -1, row) + 1
+    rest2 = mod(rest1 - 1, row) + 1
+    kr = rest2
+#     println("rests: ", rest1, " ", rest2)
+    return [kr, jr, ir]
 end
 
-bl1 = get_block_cart_calcul([10,10,10], 125)
-println(bl1)
-bl2 = get_block_cart_cartind([10,10,10], 125)
-println(bl2)
-bl3 = LarSurf.grid_voxel_id_to_cart([10,10,10], 125)
-println(bl3)
 
-function get_block_cart_cartind(blocks_number_axis, block_i)
+
+function grid_voxel_id_to_cart_cartind(blocks_number_axis, block_i)
     a = Array{Int}(
         undef,
         blocks_number_axis[1],
@@ -35,30 +35,39 @@ end
 
 
 blocks_number_axis = [10,10,10]
-a = Array{Int}(
+_cart = Array{Int}(
     undef,
     blocks_number_axis[1],
     blocks_number_axis[2],
     blocks_number_axis[3]
 )
 
-function get_block_cart_cartind_extern(blocks_number_axis, block_i)
+function grid_voxel_id_to_cart_extern(blocks_number_axis, block_i)
 #     print("block_i: ", block_i)
     # TODO maybe the cartesian index is slow
-    bsub = CartesianIndices(a)[block_i]
+    bsub = CartesianIndices(_cart)[block_i]
     return bsub
 end
 
 
+bl1 = grid_voxel_id_to_cart_calcul([10,10,10], 125)
+println(bl1)
+bl2 = grid_voxel_id_to_cart_cartind([10,10,10], 125)
+println(bl2)
+# bl3 = LarSurf.grid_voxel_id_to_cart([10,10,10], 125)
+bl3 = grid_voxel_id_to_cart_extern([10,10,10], 125)
+println(bl3)
 
-bm = @benchmark get_block_cart_cartind([10,10,10], 472)
+
+bm = @benchmark grid_voxel_id_to_cart_calcul([10,10,10], 472)
 display(bm)
 
-bm = @benchmark get_block_cart_calcul([10,10,10], 472)
+bm = @benchmark grid_voxel_id_to_cart_cartind([10,10,10], 472)
 display(bm)
 
-bm = @benchmark LarSurf.grid_voxel_id_to_cart([10,10,10], 472)
+bm = @benchmark grid_voxel_id_to_cart_extern([10,10,10], 472)
 display(bm)
 
-bm = @benchmark get_block_cart_cartind_extern([10,10,10], 472)
-display(bm)
+
+# bm = @benchmark LarSurf.grid_voxel_id_to_cart([10,10,10], 472)
+# display(bm)
