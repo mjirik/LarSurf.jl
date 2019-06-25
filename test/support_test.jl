80ef7329d944aab059ede88488c09bf44133a860
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-25 11:13:53 +0200

    removed unused LAR

--- test/support_test.jl
+++ test/support_test.jl
@@ -4,8 +4,9 @@ using Test
 using Logging
 using LarSurf
 using SparseArrays
-using LinearAlgebraicRepresentation
-Lar = LinearAlgebraicRepresentation
+using Io3d
+# using LinearAlgebraicRepresentation
+# Lar = LinearAlgebraicRepresentation
 # Logging.configure(level==Logging.Debug)
 
 
@@ -13,6 +14,33 @@ Lar = LinearAlgebraicRepresentation
 # include("../src/block.jl")
 
 @testset "array compare roll independent" begin
-    @test LarSurf.array_equal_roll_invariant([1,2,3,4,5], [2,3,4,5,1])
+    xystep = 1
+    zstep = 1
+    threshold = 4000;
+    pth = Io3d.datasets_join_path("medical/orig/sample-data/nrn4.pklz")
+    datap = Io3d.read3d(pth)
 
+    data3d = datap["data3d"]
+    segmentation = data3d .> threshold
+
+
+    block_size = [5,5,5]
+    basicmodel, Flin, larmodel = LarSurf.get_surface_grid_per_block(segmentation, block_size; return_all=true)
+    someV, topology = basicmodel
+    filtered_bigFV = topology[1]
+    # someV, filtered_bigFV = basicmodel
+    bigV = someV
+    # bigV, tmodel = larmodel
+    # bigVV, bigEV, bigFV, bigCV = tmodel
+    # return (bigV,[FVreduced])
+
+    # Plasm.View(basicmodel)
+    # Plasm.View((bigV,[bigVV, bigEV, filtered_bigFV]))
+
+    FV = filtered_bigFV
+
+    newBigV = LarSurf.Smoothing.smoothing_FV(bigV, FV)
+    @test typeof(newBigV) == Array
+
+    # Plasm.View((newBigV * 100,[filtered_bigFV]))
 end
