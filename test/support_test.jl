628106cb7d750a203ea582ed3c6a853f29407310
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-06-25 18:54:37 +0200

    removed Io3d

--- test/support_test.jl
+++ test/support_test.jl
@@ -4,7 +4,6 @@ using Test
 using Logging
 using LarSurf
 using SparseArrays
-using Io3d
 # using LinearAlgebraicRepresentation
 # Lar = LinearAlgebraicRepresentation
 # Logging.configure(level==Logging.Debug)
@@ -14,33 +13,6 @@ using Io3d
 # include("../src/block.jl")
 
 @testset "array compare roll independent" begin
-    xystep = 1
-    zstep = 1
-    threshold = 4000;
-    pth = Io3d.datasets_join_path("medical/orig/sample-data/nrn4.pklz")
-    datap = Io3d.read3d(pth)
+    @test LarSurf.array_equal_roll_invariant([1,2,3,4,5], [2,3,4,5,1])
 
-    data3d = datap["data3d"]
-    segmentation = data3d .> threshold
-
-
-    block_size = [5,5,5]
-    basicmodel, Flin, larmodel = LarSurf.get_surface_grid_per_block(segmentation, block_size; return_all=true)
-    someV, topology = basicmodel
-    filtered_bigFV = topology[1]
-    # someV, filtered_bigFV = basicmodel
-    bigV = someV
-    # bigV, tmodel = larmodel
-    # bigVV, bigEV, bigFV, bigCV = tmodel
-    # return (bigV,[FVreduced])
-
-    # Plasm.View(basicmodel)
-    # Plasm.View((bigV,[bigVV, bigEV, filtered_bigFV]))
-
-    FV = filtered_bigFV
-
-    newBigV = LarSurf.Smoothing.smoothing_FV(bigV, FV)
-    @test typeof(newBigV) == Array
-
-    # Plasm.View((newBigV * 100,[filtered_bigFV]))
 end
