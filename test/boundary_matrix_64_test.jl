f6850fa5386cb2f3206a7d8cf9832213fcb4ae00
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-06-12 19:32:25 +0200

    remove unused prints

--- test/boundary_matrix_64_test.jl
+++ test/boundary_matrix_64_test.jl
@@ -14,16 +14,10 @@ using LarSurf
 
     @time b3a, larmodel_a = LarSurf.get_boundary3(grid_size)
 
-    LarSurf.set_param(boundary_allow_memory=true)
-    b3b, larmodel_b = LarSurf.get_boundary3(grid_size)
 
     n_faces = LarSurf.grid_number_of_faces(grid_size)
-    # n_verts = LarSurf.grid_number_of_nodes(grid_size)
     n_voxels = prod(grid_size)
-
-    # println("boundary matrix size")
     # println("$(size(b3a)) , sum $(sum(b3a))")
-    # println("$(size(b3b))")
 
     @test sum(b3a) == 6*n_voxels # total number of nonzeros
     @test size(b3a) == (n_voxels, n_faces)
